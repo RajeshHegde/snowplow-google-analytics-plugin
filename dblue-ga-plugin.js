02df18241c1a5dfcc5320906bc8999a1731d6f99
Rajesh Hegde <rajesh.p.hegde@gmail.com>
2020-10-12 17:21:24 +0530

    update tracking code  On branch develop 	modified:   dblue-ga-plugin.js 	new file:   instructions/gtm.md 	modified:   instructions/shopify-code.html 	modified:   integrations/ga.html

--- dblue-ga-plugin.js
+++ dblue-ga-plugin.js
@@ -16,37 +16,137 @@
  */
 
 function DblueGaPlugin(tracker, config) {
-  this.endpoint = config.endpoint.substr(-1) != '/' ? config.endpoint + '/' : config.endpoint;
+  var clientId = tracker.get('clientId')
 
-  var vendor = 'com.google.analytics';
-  var version = 'v1';
-  var path = this.endpoint + 'c';
+  // Set GA client Id
+  if (typeof config.clientIdDimensionIndex === 'number') {
+    tracker.set('dimension' + config.clientIdDimensionIndex, clientId)
+  }
 
-  var sendHitTask = 'sendHitTask';
+  // Store the original custom task
+  var customTaskName = 'customTask'
+  var globalCustomTaskName = '_' + tracker.get('trackingId') + '_' + customTaskName
+  var originalCustomTask = (window[globalCustomTaskName] = window[globalCustomTaskName] || tracker.get(customTaskName))
 
-  var originalSendHitTask = tracker.get(sendHitTask);
-  tracker.set(sendHitTask, function(model) {
-    var payload = model.get('hitPayload');
-    originalSendHitTask(model);
+  // Add custom task to set custom dimensions
+  tracker.set(customTaskName, function (model) {
+    // Call original custom task
 
-    var request = new XMLHttpRequest();
-    request.open('POST', path, true);
-    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    if (typeof originalCustomTask === 'function') {
+      originalCustomTask(model)
+    }
 
-    payload += '&vendor=' + vendor + '&version=' + version;
-    request.send(payload);
-  });
+    var uniqueId = getUniqueId()
+
+    // Set hitId
+    if (typeof config.hitIdDimensionIndex === 'number') {
+      model.set('dimension' + config.hitIdDimensionIndex, uniqueId)
+    }
+
+    // Set sessionId
+    if (typeof config.sessionIdDimensionIndex === 'number') {
+      model.set('dimension' + config.sessionIdDimensionIndex, uniqueId)
+    }
+
+    // Set hit timestamp
+    if (typeof config.hitTimestampDimensionIndex === 'number') {
+      model.set('dimension' + config.hitTimestampDimensionIndex, getHitTimestamp())
+    }
+
+    // Set hit type
+    if (typeof config.hitTypeDimensionIndex === 'number') {
+      model.set('dimension' + config.hitTypeDimensionIndex, model.get('hitType'))
+    }
+
+    // Set full referrer
+    if (typeof config.referrerDimensionIndex === 'number') {
+      model.set('dimension' + config.referrerDimensionIndex, document.referrer)
+    }
+  })
+
+  if (config.endpoint) {
+    var endpoint = config.endpoint.substr(-1) != '/' ? config.endpoint + '/' : config.endpoint
+
+    var vendor = 'com.google.analytics'
+    var version = 'v1'
+    var path = endpoint + 'c'
+
+    // Store original send hit task
+    var sendHitTask = 'sendHitTask'
+
+    var globalSendTaskName = '_' + tracker.get('trackingId') + '_' + sendHitTask
+
+    var originalSendHitTask = (window[globalSendTaskName] = window[globalSendTaskName] || tracker.get(sendHitTask))
+
+    // Set Dblue send hit task for parallel tracking
+    tracker.set(sendHitTask, function (model) {
+      var payload = model.get('hitPayload')
+
+      // Send original Google Analytics hit
+      originalSendHitTask(model)
+
+      // Send hit to Dblue Parallel Tracker
+      var request = new XMLHttpRequest()
+      request.open('POST', path, true)
+      request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+
+      payload += '&vendor=' + vendor + '&version=' + version
+      request.send(payload)
+    })
+  }
+}
+
+function getHitTimestamp() {
+  // Get local time as ISO string with offset at the end
+  var now = new Date()
+  var tzo = -now.getTimezoneOffset()
+  var dif = tzo >= 0 ? '+' : '-'
+  var pad = function (num) {
+    var norm = Math.abs(Math.floor(num))
+    return (norm < 10 ? '0' : '') + norm
+  }
+  return (
+    now.getFullYear() +
+    '-' +
+    pad(now.getMonth() + 1) +
+    '-' +
+    pad(now.getDate()) +
+    'T' +
+    pad(now.getHours()) +
+    ':' +
+    pad(now.getMinutes()) +
+    ':' +
+    pad(now.getSeconds()) +
+    '.' +
+    pad(now.getMilliseconds()) +
+    dif +
+    pad(tzo / 60) +
+    ':' +
+    pad(tzo % 60)
+  )
+}
+
+function getUniqueId() {
+  var d = new Date().getTime()
+  if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+    d += performance.now() //use high-precision timer if available
+  }
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0
+    d = Math.floor(d / 16)
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
+  })
 }
 
 function providePlugin(pluginName, pluginConstructor) {
-  var ga = getGA();
+  var ga = getGA()
+
   if (typeof ga == 'function') {
-    ga('provide', pluginName, pluginConstructor);
+    ga('provide', pluginName, pluginConstructor)
   }
 }
 
 function getGA() {
-  return window[window['GoogleAnalyticsObject'] || 'ga'];
+  return window[window['GoogleAnalyticsObject'] || 'ga']
 }
-
-providePlugin('dblueGaPlugin', DblueGaPlugin);
+providePlugin('DblueGaPlugin', DblueGaPlugin)
